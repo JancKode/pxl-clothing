b94689ab27027ba695348386c6aa84b783cf8821
Janckode <gyntlify@gmail.com>
2019-08-29 10:20:42 +0800

    added sign up, implemented firebase utils, ability to store authenticated users in firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 
-const cofing = {
+const confing = {
   apiKey: "AIzaSyCZNgFRJeoFSHTN7IbrjvaB4gE8-xSdw9g",
   authDomain: "pxl-clothing-db.firebaseapp.com",
   databaseURL: "https://pxl-clothing-db.firebaseio.com",
@@ -12,7 +12,31 @@ const cofing = {
   appId: "1:474750880231:web:15ab727605ec3e6f"
 };
 
-firebase.initializeApp(cofing);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (e) {
+      console.log(`Error creating user`, e.message);
+    }
+  }
+  return userRef;
+};
+
+firebase.initializeApp(confing);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
